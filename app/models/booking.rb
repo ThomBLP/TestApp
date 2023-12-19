8ef972699f22807bfc8f01a53a65d00a5b82adac
Thomas Belpomo <thomas.belpomo@hotmail.fr>
2023-12-19 14:43:09 +0100

    params models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
+
+  validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, comparison: { greater_than: :today }
 end
